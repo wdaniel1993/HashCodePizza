70655f8aa6b81f2433f0b00b1ad7dc3135984dfd
wdaniel1993 <daniel.wallner@mbit.at>
2018-02-28 14:37:13 +0100

    Calculate possible Slices

--- HashCodePizza/Process/DataProcessor.cs
+++ HashCodePizza/Process/DataProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,23 @@ namespace HashCodePizza.Process
         public OutputModel Process(InputModel input)
         {
             var sliceMasks = GeneratePossibleSliceMasks(input.MinToppings*Enum.GetValues(typeof(Topping)).Length,input.MaxCells).ToList();
+            var possibleSlices = new ConcurrentBag<Slice>();
+
+            Parallel.ForEach(sliceMasks, mask =>
+            {
+                for (var i = 0; i < input.Rows - mask.Width; i++)
+                {
+                    for (var j = 0; j < input.Columns - mask.Height; j++)
+                    {
+                        var slice = new Slice(i,j,i+mask.Width,j+mask.Height);
+                        if (IsValidSlice(input,slice))
+                        {
+                            possibleSlices.Add(slice);
+                        }
+                    }
+                }
+            });
+
             var output = new OutputModel();
 
             return output;
@@ -34,6 +52,19 @@ namespace HashCodePizza.Process
                     }
                 }
             }
+        }
+
+        private bool IsValidSlice(InputModel input, Slice slice)
+        {
+            var counts = new int[Enum.GetValues(typeof(Topping)).Length];
+            for (var i = slice.StartRow; i <= slice.EndRow; i++)
+            {
+                for (var j = slice.StartColumn; j <= slice.EndColumn; j++)
+                {
+                    counts[(int) input.Pizza[i, j]]++;
+                }
+            }
+            return counts.All(x => x >= input.MinToppings);
         }
     }
 }
