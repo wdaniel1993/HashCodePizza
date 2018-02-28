eb4a56221f19c182b7d3752b65898194f14cdd58
wdaniel1993 <daniel.wallner@mbit.at>
2018-02-28 14:02:38 +0100

    Generate Slice Masks

---
+++ HashCodePizza/Models/SliceMask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodePizza.Models
+{
+    public struct SliceMask
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+}
--- HashCodePizza/Process/DataProcessor.cs
+++ HashCodePizza/Process/DataProcessor.cs
@@ -11,9 +11,29 @@ namespace HashCodePizza.Process
     {
         public OutputModel Process(InputModel input)
         {
+            var sliceMasks = GeneratePossibleSliceMasks(input.MinToppings*Enum.GetValues(typeof(Topping)).Length,input.MaxCells).ToList();
             var output = new OutputModel();
 
             return output;
+        }
+
+        private IEnumerable<SliceMask> GeneratePossibleSliceMasks(int minSize, int maxSize)
+        {
+            for (int i = 1; i <= maxSize; i++)
+            {
+                for (int j = 1; j <= maxSize; j++)
+                {
+                    var cellCount = i * j;
+                    if (minSize <= cellCount && cellCount <= maxSize)
+                    {
+                        yield return new SliceMask()
+                        {
+                            Width = i,
+                            Height = j
+                        };
+                    }
+                }
+            }
         }
     }
 }
